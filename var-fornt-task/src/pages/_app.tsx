ea3b4daa3d3a3f3bdc0c37a55bb0e576f0c8d892
Ree9o <ree9.business@gmail.com>
2023-10-04 20:14:36 +0900

    next/font 設定

--- var-fornt-task/src/pages/_app.tsx
+++ var-fornt-task/src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter,Noto_Sans_Javanese} from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+const noto_sans_japanese = Noto_Sans_Javanese({subsets:['latin']})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily},${noto_sans_japanese.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
